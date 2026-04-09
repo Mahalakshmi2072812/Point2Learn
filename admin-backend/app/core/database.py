d282de040d65b6b5d916bbe65d6194c8a27fc185
Mahalakshmi2072812 <150379141+Mahalakshmi2072812@users.noreply.github.com>
2026-04-09 16:48:03 +0530

    Update database.py

--- admin-backend/app/core/database.py
+++ admin-backend/app/core/database.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URL")
+MONGO_URL = os.getenv("MONGO_URI")
+
 DB_NAME = os.getenv("DB_NAME")
 
 # Global client variable
