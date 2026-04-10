5cf55d2779028a0c9f113b8a66d799e186d111fa
Mahalakshmi2072812 <150379141+Mahalakshmi2072812@users.noreply.github.com>
2026-04-10 14:15:53 +0530

    Update index.js

--- admin-frontend/src/api/index.js
+++ admin-frontend/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const API_BASE = import.meta.env.VITE_API_BASE
+console.log("BASE URL:", import.meta.env.VITE_API_BASE)
 const http = axios.create({ baseURL: API_BASE, timeout: 15000 })
 
 // 🔐 Attach token
